000de98b91702db5ead0d02320ccc24cfbc77b51
rez39 <30721512+rez39@users.noreply.github.com>
2017-08-21 15:16:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
 console.log('Loaded!');
 alert('i have added a js file also in to my html');
-var marginleft=0;
-var img=document.getElementById("image");
 
+var img=document.getElementById("image");
+var marginleft=0;
 function moveRight()
 {marginleft=marginleft+10;
     img.style.marginleft=marginleft+'px';
