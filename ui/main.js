b49d6201e035949437d8649fe946739b01c84162
rez39 <30721512+rez39@users.noreply.github.com>
2017-09-08 19:35:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,3 +11,10 @@ function moveRight()
 {
     var interval=setInterval(moveRight,50);
 };
+var counter=0;
+var button=documnt.getElementById('counter');
+button.onclick =function()
+{
+    counter=counter+1;
+    var span=document.getElementById('count')
+span.innerHTML= counter.toString();}
