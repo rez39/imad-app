0badd54b4ddda28b3d180fc9e8f3d1d38b14a1c6
rez39 <30721512+rez39@users.noreply.github.com>
2017-09-09 18:35:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,6 +15,19 @@ var counter=0;
 var button=document.getElementById('counter');
 button.onclick = function()
 {
-    counter=counter+1;
-    var span=document.getElementById('count');
-span.innerHTML= counter.toString();};
+    
+    var request= new XMLHTTPRequest();
+    request.onreadystatechange=function()
+    {if(request.readyState === XMLHttpRequest.DONE)
+    {
+        if(request.status===200)
+        {var counter=request.responseText;
+         var span=document.getElementById('count');
+span.innerHTML= counter.toString();   
+        }
+    }
+        };
+        request.open('GET','http://http://rajcute39.imad.hasura-app.io/counter',true);
+        request.send(null)
+        
+        };
