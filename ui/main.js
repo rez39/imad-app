8219798ca959f8faff8c481e31e59ae8061ef15e
rez39 <30721512+rez39@users.noreply.github.com>
2017-09-11 17:10:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,5 @@
-var img=document.getElementById("image");
-var marginleft=0;
+
 var button=document.getElementById('counter');
-function moveRight()
-{marginleft=marginleft+1;
-    img.style.marginLeft=marginleft+'px';
-}
-    img.onclick= function()
-{
-    var interval=setInterval(moveRight,50);
-};
 
 var button=document.getElementById('counter');
 button.onclick = function()
