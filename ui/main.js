92d41ffb7bb16d61e7e5c3814ed86f85194eb1e1
rez39 <30721512+rez39@users.noreply.github.com>
2017-09-11 19:26:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,6 @@
 var button=document.getElementById("counter");
 button.onclick = function()
-{
-    
-    var request= new XMLHttpRequest();
+{ var request= new XMLHttpRequest();
     request.onreadystatechange=function()
     {
         if(request.readyState === XMLHttpRequest.DONE)
@@ -17,14 +15,23 @@ button.onclick = function()
         };
         request.open('GET','http://rajcute39.imad.hasura-app.io/counter',true);
         request.send(null);
+    
+   
         
         };
 var nameInput = document.getElementById('name')
 var name =nameInput.value;
 var submit= document.getElementById('submit-btn')
 submit.onclick = function()
-{
-    var names= ['name1','name2','name3']
+{ var request= new XMLHttpRequest();
+    request.onreadystatechange=function()
+    {
+        if(request.readyState === XMLHttpRequest.DONE)
+    
+    {
+        if(request.status === 200)
+        { var names= request.responseText;
+        names=JSON.parse(names)
     var list =  '';
     for(var i=0;i<names.length;i++)
     {
@@ -32,5 +39,11 @@ submit.onclick = function()
    
     }
     var ul =document.getElementById('namelist');
-    ul.innerHTML = list;
+    ul.innerHTML = list;  
+        }
+      }
+        };
+        request.open('GET','http://rajcute39.imad.hasura-app.io/submit-name?name=' + name,true);
+        request.send(null);
+    
 }
