b9efe05a8a3c923f23c6e82addd8db3d25b9b670
rez39 <30721512+rez39@users.noreply.github.com>
2017-09-13 19:30:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool= require('pg').Pool;
+var crypto= require('crypto');
 
 var app = express();
 app.use(morgan('combined'));
@@ -58,7 +60,18 @@ var htmltemplate =`<!DOCTYPE html>
     return htmltemplate;
     
 }
-
+function hash(input,salt)
+{
+    var hashed= crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    
+    return hashed.toString('hex')
+}
+app.get('/hash/:input',function(req,res)
+{
+   var hashedString = hash(req.params.input,'this is a random-string');
+   res.send(hashedString);
+   
+})
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
