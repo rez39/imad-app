b0dd234aabc960fc7e6a3069e0e708ad276bada8
rez39 <30721512+rez39@users.noreply.github.com>
2017-09-13 19:47:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ function hash(input,salt)
 {
     var hashed= crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
     
-    return hashed.toString('hex')
+    return ['pbkdf2Sync','this is a random-string',10000,hashed.toString('hex')].join('$')
 }
 app.get('/hash/:input',function(req,res)
 {
